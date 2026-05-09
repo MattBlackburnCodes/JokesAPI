ed7bec488ecd6b3380f83c186f646e4cf292ba0f
SoliBlack <SolitaireBlack21@gmail.com>
2026-05-08 20:32:27 -0400

    Adjusted CORS

--- api/jokes.js
+++ api/jokes.js
@@ -1,7 +1,11 @@
 export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization"
+    );
 
 
     if (req.method === 'OPTIONS') {
